83724d4442c58a60af04d8a3e372c1a255aec8a9
Slimakoi <slimeytoficial@gmail.com>
2020-10-11 15:38:19 +0100

    Release 1.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Slimakoi'
 description = 'A library to create Amino bots.'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0a'
+release = '1.2.1'
 
 # -- General configuration ---------------------------------------------------
 
